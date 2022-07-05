6e492c0e03ca7c63311a6443f2465371533229fc
Ana Torres <ana.beatriz_19@hotmail.com>
2022-07-04 21:25:50 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,33 @@
+public class C {
+    private String C1;
+    private int C2;
+
+    public C (String C1, int C2) {
+        this.C1 = C1;
+        this.C2 = C2;
+    }
+
+    public String getC1() {
+        return C1;
+    }
+
+    public void setC1(String a1) {
+        C1 = a1;
+    }
+
+    public int getC2() {
+        return C2;
+    }
+
+    public void setC2(int a2) {
+        C2 = a2;
+    }
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
